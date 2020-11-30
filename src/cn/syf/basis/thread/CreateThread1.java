2ce1c9ee52e258df585ffb4a63acaf3e9cb7e54b
shengyifan <shengyifan@tongqutech.cn>
2020-11-30 18:40:40 +0800

    线程

---
+++ src/cn/syf/basis/thread/CreateThread1.java
@@ -0,0 +1,11 @@
+package cn.syf.basis.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class CreateThread1 {
+
+    public static void main(String[] args) throws Exception {
+    }
+}
